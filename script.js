065c726651e39344496740fe32bab5d9ebdb75d5
dhika827 <dika00498@gmail.com>
2026-08-28 07:58:55 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const kamu = {
-    senyum: "Manis",
-    hadirmu: "Menenangkan",
-    status: "Tidak tergantikan"
-};
-
-if (kamu.status === "Tidak tergantikan") {
-    alert("Terima kasih ya sudah ada dan bikin hariku selalu cerah! 💕");
-}

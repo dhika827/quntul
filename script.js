ebf818cda706172c39c7dc9f1341288d47806f0d
dhika827 <dika00498@gmail.com>
2026-08-28 07:55:55 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const kamu = {
+    senyum: "Manis",
+    hadirmu: "Menenangkan",
+    status: "Tidak tergantikan"
+};
+
+if (kamu.status === "Tidak tergantikan") {
+    alert("Terima kasih ya sudah ada dan bikin hariku selalu cerah! 💕");
+}
